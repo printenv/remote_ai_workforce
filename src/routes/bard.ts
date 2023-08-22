216fbda666b11d54bc5ffb9da853ccd11e717785
printenv <printenv.run@icloud.com>
2023-08-22 21:25:26 +0900

    delete bard

--- src/routes/bard.ts
+++
@@ -1,9 +0,0 @@
-import express, {Request, Response} from 'express'
-
-const route = express.Router()
-
-route.get("/test", (req:Request, res:Response) => {
-    res.json("bard route")
-})
-
-export default route
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import express, {Request, Response} from 'express'
 import errorHandler from "./midlleware/errorHandler"
 import openaiRoute from './routes/openai'
 import azureRoute from "./routes/azure"
-import bardRoute from './routes/bard'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,7 +11,6 @@ app.use(errorHandler)
 
 app.use("/api/openai/", openaiRoute)
 app.use("/api/azure", azureRoute)
-app.use("/api/bard/", bardRoute)
 
 app.listen(port, ()=>{
     console.log(`Server running on https://localhost:${port}`)
