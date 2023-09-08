c8bc59650f84dfb7a3c3f6f2bb0aaafd2ad3b49d
Takaaki <printenv.run@icloud.com>
2023-09-08 20:19:33 +0900

    prepare for CI/CD

--- dist/server.js
+++ dist/server.js
@@ -4,12 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const errorHandler_1 = __importDefault(require("./midlleware/errorHandler"));
+const openai_1 = __importDefault(require("./routes/openai"));
+const azure_1 = __importDefault(require("./routes/azure"));
+const wp_1 = __importDefault(require("./routes/wp"));
+const init_1 = __importDefault(require("./wp/init"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.use(express_1.default.json());
-app.get('/api/test', (req, res) => {
-    res.json("OK");
-});
+app.use("/api/openai/", openai_1.default);
+app.use("/api/azure", azure_1.default);
+app.use("/api/wp", wp_1.default);
+app.use(errorHandler_1.default);
+(0, init_1.default)();
 app.listen(port, () => {
     console.log(`Server running on https://localhost:${port}`);
 });
--- readme.md
+++ readme.md
@@ -6,3 +6,14 @@
 - WP_NAMIHEI_PASSWORD
 - WP_MORTY_PASSWORD
 - OPENAI_API_KEY
+
+## CI/CD Setup
+### Setup Secretes and variables (Actions) bellow on github
+- APP_PATH : App's directory path on VPS
+- DEPLOY_KEY: SSH private key that matches public key set up on VPS
+- HOST: VPS IP address
+- PORT: SSH port on VPS
+- USERNAME: VPS user name
+
+
+
